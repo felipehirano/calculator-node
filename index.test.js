bd43a4224413d1166fdbcb27be9d52e013322506
Felipe Ken <fkenhirano4@gmail.com>
2026-02-15 16:53:12 -0300

    ci: Ajusts on ci.yaml

--- index.test.js
+++ index.test.js
@@ -2,6 +2,6 @@ const { sum } = require('./index.js');
 
 describe('Math functions', () => {
   test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3);
+    expect(sum(1, 2)).toBe(2);
   });
 });
